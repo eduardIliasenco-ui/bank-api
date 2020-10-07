0f4e1eef2e2cc8b58166cf068e4691fa1f85f3ea
Eduard Iliasenco <eduard.iliasenco@endava.com>
2020-10-07 10:18:39 +0300

    validation pipe added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,12 @@ import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   
   setupSwagger(app);
   await app.listen(3003);
